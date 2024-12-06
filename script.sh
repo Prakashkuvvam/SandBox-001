47eecd161a186274a52a9b5a8af1b75d5edae4a6
Prakashkuvvam <108862967+Prakashkuvvam@users.noreply.github.com>
2024-12-06 09:00:09 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
-'''#!/bin/bash
+#!/bin/bash
 npm install express body-parser
 npm init -y
 npm install xterm ws
-'''
+
 ### To run the server
- '''node backend/server.js'''
+ node backend/server.js
 
 
 
